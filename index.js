778223fa6cb19f53143f3834d5c95fc334c0d75d
Sam Dunster <sdunster@fb.com>
2015-12-19 17:34:57 -0800

    Remove logging, bump to 1.0.1

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ var _ = require('underscore');
 
 module.exports = function(tag, attributes, children) {
   var elem = document.createElement(tag);
-  console.log(tag);
 
   _.each(attributes, function(val, key) {
-    console.log("set attribute " + key + " to " + val);
     elem.setAttribute(key, val);
   });
 
@@ -14,7 +12,6 @@ module.exports = function(tag, attributes, children) {
       elem.appendChild(document.createTextNode(child));
     }
     else {
-      console.log(child);
       elem.appendChild(child);
     }
   });
