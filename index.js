1444e4b424e3edd63b5102b7edff7c98d75f79ab
Sam Dunster <sdunster@fb.com>
2015-12-22 00:17:50 -0800

    Add support for children Array

--- index.js
+++ index.js
@@ -7,13 +7,24 @@ module.exports = function(tag, attributes, children) {
     elem.setAttribute(key, val);
   });
 
-  _.each(children, function(child) {
-    if(typeof child === 'string') {
+  // in-order traversal of sub-arrays
+  var handle_child = function(child) {
+    if(child.constructor === Array) {
+      _.each(child, function(c) {
+        handle_child(c);
+      });
+    }
+    else if(typeof child === 'string') {
       elem.appendChild(document.createTextNode(child));
     }
     else {
       elem.appendChild(child);
     }
+  };
+
+  // children must be an array so unwrap the first level
+  _.each(children, function(child) {
+    handle_child(child);
   });
 
   return elem;
