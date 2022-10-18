73ecd6d081a09d4cf44e8c7f548a97704d79e46c
Herman Lederer <germans.lederers@gmail.com>
2022-10-18 15:46:05 +0200

    Remove underscore

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import _ from 'underscore';
-
-export default function createElement(tag, attributes, ...children) {
+const createElement = (tag, attributes, ...children) => {
   const elem = document.createElement(tag);
 
-  _.each(attributes, (val, key) => {
-    elem.setAttribute(key, val);
+  Object.entries(attributes ?? {}).forEach(([key, value]) => {
+    elem.setAttribute(key, value);
   });
 
   // in-order traversal of sub-arrays
-  function handleChild(child) {
+  const handleChild = (child) => {
     if (child.constructor === Array) {
-      _.each(child, (c) => {
+      child.forEach((c) => {
         handleChild(c);
       });
     } else if (typeof child === 'string') {
@@ -18,13 +16,15 @@ export default function createElement(tag, attributes, ...children) {
     } else {
       elem.appendChild(child);
     }
-  }
+  };
 
   // children could be a single argument that is an Array or it could be
   // multiple arguments depending on the JSX version
-  _.each(children, (child) => {
+  children.forEach((child) => {
     handleChild(child);
   });
 
   return elem;
-}
+};
+
+export default createElement;
